a66be342f4e5b05647264b673711da9f056ebe92
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-01-25 11:27:19 +0100

    lekcja3
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- lekcja1.py
+++ lekcja1.py
@@ -6,8 +6,11 @@ def main():
    run=True
    while(run):
        pygame.time.delay(200)
+       wazShape=pygame.Rect((140,140),(47,47))
+       pygame.draw.rect(OknoGry,(255,192,203),wazShape)
        for event in pygame.event.get():
            if event.type == pygame.QUIT:
                run=False
+       pygame.display.update()
 
 main() 
