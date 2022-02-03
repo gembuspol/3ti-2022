905d231b162b6fe4d49ec4f83fdf98b1d526fd9c
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-02-03 08:47:27 +0100

    lekcja5
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- lekcja1.py
+++ lekcja1.py
@@ -9,8 +9,11 @@ def main():
    while(run):
        OknoGry.fill((0,0,0))
        pygame.time.delay(200)
-       wazShape=pygame.Rect((zmienna1,zmienna2),(47,47))
+       #rysowanie weza
+       wazShape=pygame.Rect((zmienna1,zmienna2),(40,40))
        pygame.draw.rect(OknoGry,(255,192,203),wazShape)
+        #rysowanie jabłka
+       pygame.draw.circle(OknoGry,(0,255,0),(100,100),20)
        for event in pygame.event.get():
            if event.type == pygame.QUIT:
                run=False
@@ -20,12 +23,24 @@ def main():
                    zmienna2=zmienna2-20
                elif event.key == pygame.K_DOWN:
                    zmienna2=zmienna2+20
+               elif event.key == pygame.K_LEFT:
+                   zmienna1=zmienna1-20
+               elif event.key == pygame.K_RIGHT:
+                   zmienna1=zmienna1+20
        #zmienna1=zmienna1+20
        #zmienna2=zmienna2+20
+       #przejście strona prawa
        if zmienna1>400:
            zmienna1=0
+        #przejście dół
        if zmienna2>400:
            zmienna2=0
+       #przejście strona lewa
+       if zmienna1<0:
+           zmienna1=400
+        #przejście góra
+       if zmienna2<0:
+           zmienna2=400    
        pygame.display.update()
 
 main() 
