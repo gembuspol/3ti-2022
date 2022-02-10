467ade38c483352a84c9db06078bfc6fc62be26a
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-02-10 08:48:35 +0100

    zwiększanie weża
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- lekcja1.py
+++ lekcja1.py
@@ -2,6 +2,7 @@ import pygame
 import random
 
 def main():
+    dlugoszWeza=1
     xApple=random.randint(0,9)*40+20
     yApple=random.randint(0,9)*40+20
     pygame.init()
@@ -9,14 +10,16 @@ def main():
     run=True
     zmienna1=120
     zmienna2=120
+    #przypisanie pierwszej pozycji węża
+    pozycjaWaz=[(zmienna1,zmienna2)]
+    pozycjaWaz.append((160,120))
     while(run):
-        pozycja=[1,2,3,5,7,"a","d"]
-        print("To jest tekst:",pozycja)
         OknoGry.fill((0,0,0))
         pygame.time.delay(200)
         #rysowanie weza
-        wazShape=pygame.Rect((zmienna1,zmienna2),(40,40))
-        pygame.draw.rect(OknoGry,(255,192,203),wazShape)
+        for wspolrzendne in pozycjaWaz[::-1]: 
+            wazShape=pygame.Rect((wspolrzendne[0],wspolrzendne[1]),(40,40))
+            pygame.draw.rect(OknoGry,(255,192,203),wazShape)
             #rysowanie jabłka
         pygame.draw.circle(OknoGry,(0,255,0),(xApple,yApple),20)
         for event in pygame.event.get():
@@ -32,11 +35,16 @@ def main():
                     zmienna1=zmienna1-40
                 elif event.key == pygame.K_RIGHT:
                     zmienna1=zmienna1+40
+        #dodawanie nowej pozycji węża
+        pozycjaWaz.append((zmienna1,zmienna2))
+        if dlugoszWeza<len(pozycjaWaz):
+            del pozycjaWaz[0]
         #waż zjada jabłko
         if zmienna1==xApple-20 and zmienna2==yApple-20:
             xApple=random.randint(0,9)*40+20
             yApple=random.randint(0,9)*40+20
             pygame.draw.circle(OknoGry,(255,255,0),(xApple,yApple),20)
+            dlugoszWeza+=1
        #zmienna1=zmienna1+20
        #zmienna2=zmienna2+20
        #przejście strona prawa
