629555f8fd1ff11d2767d0383cd8f328dd83c569
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-04-05 11:46:20 +0200

    ruch weza
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- waz.py
+++ waz.py
@@ -7,6 +7,7 @@ class Snake():
         self.dlugosc=1
         self.punkty=0
         self.pozycje=[(120,120)]
+    #pobranie pozycji głowy
     def getHead(self):
         return self.pozycje[-1]
     def eating(self):
@@ -16,3 +17,30 @@ class Snake():
          for wspolrzendne in self.pozycje[::-1]: 
             wazShape=pygame.Rect((wspolrzendne[0],wspolrzendne[1]),(40,40))
             pygame.draw.rect(OknoGry,(255,192,203),wazShape)
+    def snakeMove(self,x,y):
+        #sprawdzenie przejścia krawędzi
+        noweWspl=self.checkBorder(x,y)
+         #sprawdzenie czy wąż sam siebie nie zjadł
+        for wspol in self.pozycje[::]:
+            if noweWspl[0]==wspol[0] and noweWspl[1]==wspol[1]:
+                self.pozycje=[]
+                self.dlugosc=1
+                self.punkty=0
+        #dodanie nowej pozycji weza
+        self.pozycje.append(noweWspl[0],noweWspl[1])
+        if self.dlugosc<len(self.pozycje):
+            del self.pozycje[0]
+    #sprawdzenie krawędzi
+    def checkBorder(self,zmienna1,zmienna2):
+        if zmienna1>=400:
+            zmienna1=0
+            #przejście dół
+        if zmienna2>=400:
+            zmienna2=0
+        #przejście strona lewa
+        if zmienna1<0:
+            zmienna1=400
+            #przejście góra
+        if zmienna2<0:
+            zmienna2=400
+        return (zmienna1,zmienna2)   
