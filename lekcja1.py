24f8d9175d321b38e89ea6f12ca3c6f4109c6435
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-05-11 14:59:50 +0200

    rozdzielczosc
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- lekcja1.py
+++ lekcja1.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import waz
 import jablko
+rozdzielczosc=400
 
 iloscJablek=9
 def main():
@@ -15,7 +16,7 @@ def main():
     
     
     pygame.init()
-    OknoGry=pygame.display.set_mode((400,400),0,32)
+    OknoGry=pygame.display.set_mode((rozdzielczosc,rozdzielczosc),0,32)
     run=True
     zmienna1=120
     zmienna2=120
